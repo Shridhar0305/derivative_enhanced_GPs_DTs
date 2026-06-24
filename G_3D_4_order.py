805b51e6275e585011a5704380f53b1443ded5be
Shridhar Vashishtha <82993782+Shridhar0305@users.noreply.github.com>
2026-06-24 17:17:56 -0600

    Refactor import statements in G_3D_4_order.py

--- G_3D_4_order.py
+++ G_3D_4_order.py
@@ -2,12 +2,12 @@ import numpy as np
 import pandas as pd
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from SGP.Cov import GaussianCovariance_generic
-from SGP.Factors import ExplicitKLFactorization,ImplicitKLFactorization
-from SGP.meas import PointMeasurement,dPointMeasurement,ddPointMeasurement,dddPointMeasurement,ddddPointMeasurement
-from SGP.Cov_g import gaussian_cov_generic as g
-from SGP.supernode_converter import convert_measurements_to_list_of_dicts
+# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from Cov import GaussianCovariance_generic
+from Factors import ExplicitKLFactorization,ImplicitKLFactorization
+from meas import PointMeasurement,dPointMeasurement,ddPointMeasurement,dddPointMeasurement,ddddPointMeasurement
+from Cov_g import gaussian_cov_generic as g
+from supernode_converter import convert_measurements_to_list_of_dicts
 import scipy.sparse.linalg as lg
 import argparse
 import time
